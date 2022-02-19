4a356b0745a8ff04b9a8f281df28ebbe470f6e26
wandersonce <wandersonce1@hotmail.com>
2022-02-18 20:14:02 -0800

    Sanity auth client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react';
+import { client} from '../lib/sanityClient'
 
 const style = {
   wrapper: ``,
   walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
-  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
-  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+  button: `border border-[#282b2f] bg-[#673D4D] p-[0.8rem] text-xl text-[#fff] font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#fff] font-semibold mt-4`,
 }
 
 export default function Home() {
-  const { address, connectWallet} = useWeb3()
+  const { address, connectWallet} = useWeb3();
+
+  useEffect(() => {
+    if (!address) return
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        userName: 'Unnamed',
+        walletAddress: address,
+      }
+
+      const result = await client.createIfNotExists(userDoc)
+    })()
+  }, [address])
+
   return (
     <div className={style.wrapper}>
       {address ? (
